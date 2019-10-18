ff5f5a8c12e2f2e9fd6fa28a1a09c05df81a08c2
Brittany Huber <brittanyleighhuber@gmail.com>
2019-10-17 22:59:03 -0600

    Multiple changes added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,125 @@
-//Define the array
+//Variables and arrays
 var pokemon = [
     "Squirtle",
     "Pikachu",
-    "Clefairy"
-
+    "Seel"
 ];
 
+var randomPokemon = "";
+var lettersInPokemon = []
+var blanks = 0;
+var blanksWithGuesses = [];
+var wrongGuess = [];
 
-//Page gives a blacked out Pokemon image and displays the number of characters in the Pokemon's name
-function choosePokemon() {
-    var pokemonChoice = Math.floor(Math.random() * pokemon.length);
-    return pokemon[pokemonChoice];
-}
+var wins = 0;
+var guessesLeft = 10;
 
-function wordLength(pokemonWord) {
-    var answerArray = [];
-    for (var i = 0; i < pokemonWord.length; i++) {
-        answerArray[i] = "_";
+//Functions
+
+
+//Global function
+function Game() {
+    //Select random Pokemon word
+    randomPokemon = pokemon[Math.floor(Math.random() * pokemon.length)];
+    lettersInPokemon = randomPokemon.split("");
+    blanks = lettersInPokemon.length;
+
+    for (var i = 0; i < blanks; i++) {
+        blanksWithGuesses.push("_");
     }
-    return answerArray;
+
+    document.getElementById("whos-that-text").innerHTML = " " + blanksWithGuesses.join(" ");
+
+    //testing with console.log
+    console.log(randomPokemon);
+    console.log(lettersInPokemon);
+    console.log(blanks);
 }
 
-console.log(wordLength(choosePokemon()));
+//Function to set a blank image to be guessed
+function blankImage() {
+    //Squirtle
+    if (randomPokemon === pokemon[0]) {
+        document.getElementById(".image").src = "./assets/images/BlankSquirtle.jpg";
+    }
 
+    //Pikachu
+    else if (randomPokemon === pokemon[1]) {
+        document.getElementById(".image").src = "./assets/images/BlankPikachu.jpg";
+    }
 
+    //Seel
+    else if (randomPokemon === pokemon[2]){
+        document.getElementById(".image").src = "./assets/images/BlankSeel.jpg";
+    }
 
+}
 
+//Reset function for when game is won/lost
+function reset() {
+    guessesLeft = 10;
+    wrongGuess = [];
+    blanksWithGuesses = [];
+    Game()
+}
 
-//User presses keys to enter their guesses for who the Pokemon is. The app records the choices they get correct and keeps track of the letters they've guessed
+//Functions to determine letters in the Pokemon chosen
 
+function checkGuess(letter) {
+    var letterInGuess = false;
+    for (var i=0; i < blanks; i++) {
+        if (randomPokemon[i] == letter) {
+            letterInGuess = true;
+        }
+    }
 
+    if (letterInGuess) {
+        for (var i = 0; i < blanks; i++) {
+            if (randomPokemon[i] == letter) {
+                blanksWithGuesses[i] = letter;
+            }
+        }
+    }
 
+    else {
+        wrongGuess.push(letter);
+        guessesLeft--;
+    }
+}
 
+//Wins and losses functions - show full Pokemon image
 
-//The app awards users 6 missed letters before the game is over. The app counts down their guesses remaining
+function endGame() {
+    if (lettersInPokemon.toString() == blanksWithGuesses.toString()) {
+        wins++;
+        if (randomPokemon === pokemon[0]) {
+            document.getElementById("image").src= "./assets/images/Squirtle.jpg";
+        }
+        if (randomPokemon === pokemon[1]) {
+            document.getElementById("image").src = "./assets/images/Pikachu.jpg";
+        }
+        if (randomPokemon === pokemon[2]) {
+            document.getElementById("image").src = "./assets/images/Seel.jpg";
+        }
+        reset()
+    }
 
+    else if (guessesLeft === 0) {
+        losses++;
+        reset()
+    }
+}
 
+//Calling the function, adding functions to check which keys are pressed
+Game()
 
+document.onkeyup = function (event) {
+    var guesses = event.key.toLowerCase();
+    checkGuess(guesses);
+    endGame();
 
+    console.log(guesses);
 
+    document.getElementById("letters-guessed-text").innerHTML = " " + wrongGuess.join(" ");
+}
 
-//When all the letters have been guessed correctly, the image will turn to the actual image of the Pokemon
